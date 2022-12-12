0d79de005db573a9f82a0814b856df612f379632
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-12 18:55:14 +0000

    Add todo view

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+# from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
+from core.permissions import IsOwnerOrReadOnly
+from .models import Todo
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class TodoList(generics.ListCreateAPIView):
+    """
+    List all todos.
+    """
+
+    queryset = Todo.objects.all()
+    serializer_class = ProfileSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
+
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Todo.objects.all()
+    serializer_class = ProfileSerializer
