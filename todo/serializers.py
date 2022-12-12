d11988a056827a82da7b93e7566f2a25ffc2d6c4
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-12 19:40:26 +0000

    Add status to todo serializer

--- todo/serializers.py
+++ todo/serializers.py
@@ -20,6 +20,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
             "title",
+            "status",
             "content",
             "priority",
             "is_owner",
