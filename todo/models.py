4ac5bbcd03b1d64a0f4afd6be3b8a68ae878c84c
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-12 18:47:07 +0000

    Add todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+todo_choices = [
+    ("todo", "Todo"),
+    ("in_progress", "In progress"),
+    ("on_hold", "On hold"),
+    ("done", "Done"),
+]
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    priority = models.CharField(max_length=255, default="todo", choices=todo_choices)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to="images/", blank=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
