424f891ef0d1625216d12ff9e775cb9b155879fe
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-12 20:32:32 +0000

    Update todo field with new name in comment model

--- comment/models.py
+++ comment/models.py
@@ -9,7 +9,7 @@ class Comment(models.Model):
     """
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    Todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
