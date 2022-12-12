ac3c04f6f317442814938099878a5fe40bdc9d8d
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-12 19:08:15 +0000

    Update updated_at in profile and todo models

--- todo/models.py
+++ todo/models.py
@@ -15,7 +15,7 @@ class Todo(models.Model):
     priority = models.CharField(max_length=255, default="todo", choices=todo_choices)
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     file = models.FileField(upload_to="images/", blank=True)
 
     class Meta:
