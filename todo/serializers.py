6ef4125f75849278e74c3188b329d51c6ec1f88a
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-12 18:55:02 +0000

    Add todo serializer

---
+++ todo/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Todo
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context["request"]
+        return request.user == obj.owner
+
+    class Meta:
+        model = Todo
+        fields = [
+            "id",
+            "owner",
+            "created_at",
+            "updated_at",
+            "title",
+            "content",
+            "priority",
+            "is_owner",
+            "file",
+        ]
