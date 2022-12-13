a50e677350fcf7c2aa3636485d09d53d1913f31b
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-13 17:12:08 +0000

    Add datetime format to rest framework

--- core/settings.py
+++ core/settings.py
@@ -20,6 +20,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DATETIME_FORMAT": "%Y %b %d",
 }
 
 if "DEV" not in os.environ:
