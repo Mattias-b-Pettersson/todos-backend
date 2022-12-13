9d07fab99dcee378b2eaf465a2e98df88b90e762
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-13 17:29:12 +0000

    Remove commented out code

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+
 # from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, filters
 from core.permissions import IsOwnerOrReadOnly
@@ -11,30 +12,16 @@ class ProfileList(generics.ListAPIView):
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
-    # queryset = Profile.objects.annotate(
-    #     posts_count=Count("owner__post", distinct=True),
-    #     followers_count=Count("owner__followed", distinct=True),
-    #     following_count=Count("owner__following", distinct=True)
-    # ).order_by("-created_at")
-    serializer_class = ProfileSerializer
-    # filter_backends = [
-    #     filters.OrderingFilter,
-    #     filters.SearchFilter,
-    #     DjangoFilterBackend
-    # ]
-    # filterset_fields = [
-    #     "owner__following__followed__profile"
-    # ]
-    # ordering_fields = [
 
-    # ]
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     Retrieve or update a profile if you're the owner.
     """
+
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
