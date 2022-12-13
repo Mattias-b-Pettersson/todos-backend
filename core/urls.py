f3f02d21b1dadf98f09f2dec6e435d591d90c8e5
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-13 17:02:28 +0000

    Add Allauth and dj_rest_auth

--- core/urls.py
+++ core/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path("", include("todo.urls")),
     path("", include("comment.urls")),
     path("auth/", include("rest_framework.urls")),
+    path("dj-rest-auth/", include("dj_rest_auth.urls")),
+    path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
 ]
