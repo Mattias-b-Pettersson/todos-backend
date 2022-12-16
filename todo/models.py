64da8c8f32f30aca5ad13c0ff4f0c54d3a6d57d7
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-16 17:41:59 +0000

    Add null to true for file in todo model

--- todo/models.py
+++ todo/models.py
@@ -25,7 +25,7 @@ class Todo(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    file = models.FileField(upload_to="images/", blank=True)
+    file = models.FileField(upload_to="images/", blank=True, null=True)
     assigned = models.ManyToManyField(User, related_name="assigned")
 
     class Meta:
