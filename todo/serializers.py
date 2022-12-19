35f533fdf8b5cf2b9c800b8400dd0bf759dc78b2
Mattias-b-Pettersson <mattep3@gmail.com>
2022-12-19 18:51:32 +0000

    Update status display massage that's sent with req

--- todo/serializers.py
+++ todo/serializers.py
@@ -10,6 +10,19 @@ class TodoSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source="owner.image")
     due_date_has_passed = serializers.SerializerMethodField()
     assigned_username = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+
+    def get_status(self, obj):
+        if obj.status == "on_hold":
+            return "On Hold"
+        elif obj.status == "in_progress":
+            return "In Progress"
+        elif obj.status == "done":
+            return "Done"
+        elif obj.status == "todo":
+            return "Todo"
+        else:
+            return obj.status
 
     def get_assigned_username(self, obj):
         return obj.assigned.values_list("username", flat=True)
